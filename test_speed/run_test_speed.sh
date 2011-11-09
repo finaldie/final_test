5eea12ad36a8ef7b5b9dcfdb046b58ecb02830c8
huyuzhang <hyzwowtools@163.com>
2011-11-09 03:42:15 -0800

    mv test_json & test_pb into test_speed

--- test_speed/run_test_speed.sh
+++ test_speed/run_test_speed.sh
@@ -4,12 +4,12 @@ JSON_DIR = ../test_json/
 PB_DIR = ../test_protobuf/
 RES = result.log
 
-if ( [ -f result.log ] ); then
-    rm -f result.log
+if ( [ -f $RES ] ); then
+    rm -f $RES
 fi
 
 echo "speed test start" >> $RES
 echo "name      |count  |serialize  |unserialize    |avg    |radio" >> $RES
-JSON_DIR/test >> $RES
-PB_DIR/test >> $RES
+$JSON_DIR/test >> $RES
+$PB_DIR/test >> $RES
 echo "speeded test end" >> $RES
