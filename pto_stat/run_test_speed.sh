62f9daba21388ebe95dc55a4096ffbe2e861e1c7
huyuzhang <hyzwowtools@gmail.com>
2011-11-09 08:40:04 -0800

    resize the format of print of run_test_speed.sh

--- pto_stat/run_test_speed.sh
+++ pto_stat/run_test_speed.sh
@@ -14,12 +14,11 @@ echo "test sample: c struct{ int32 id = 1, char name[] = \"hyz\"}" >> $RES
 echo "unit of time: usec, unit of lengh: byte" >> $RES
 echo "compiler: g++ v4.1.2" >> $RES
 echo "CPPFLAGS: -O2" >>$RES
-echo "librarys: jsoncpp-0.5.0.4, google protobuf--last stable v2.4" >> $RES
+echo "librarys: jsoncpp-0.5.0.4, google protobuf last stable v2.4" >> $RES
 echo "" >> $RES
 
-echo "speed test start" >> $RES
-echo "name      |count     |serialize |unserialize|total     |ser_per   |unser_per |size      |radio" >> $RES
+echo "statistics result" >> $RES
+echo "name      |count     |serialize |unserialize|total     |ser/per   |unser/per |size      |radio" >> $RES
 $JSON_DIR/test $COUNT >> $RES
 $PB_DIR/test $COUNT >> $RES
-echo "speeded test end" >> $RES
 rm -f $TMP
