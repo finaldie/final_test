ec3b3697cb103b441f2eb850abbf01b0b8ddcef3
huyuzhang <hyzwowtools@gmail.com>
2012-07-20 01:56:31 -0700

    refine test code for flog

--- log/main.c
+++ log/main.c
@@ -19,14 +19,25 @@ void* write_log(void* arg)
     return NULL;
 }
 
+// cmd: ./test 100000 3
+// note: first param - count of log msg
+//       second param - how many threads we start
 int main(int argc, char** argv)
 {
     if ( argc < 2 ) {
         printf("please input a number for logging count\n");
+        printf("cmd format: ./test msg_count [ thread_num ]\n");
         exit(1);
     }
 
     int num = atoi(argv[1]);
+
+    // default thread num = 2
+    int thread_num = 2;
+    // get thread num
+    if ( argc == 3 ) {
+        thread_num = atoi(argv[2]);
+    }
 
     printf("startup...\n");
     log_handler = flog_create("single_thread");
@@ -42,16 +53,18 @@ int main(int argc, char** argv)
 
     sleep(2);
 
-    printf("start multip testing...\n");
+    printf("start multip testing ( totally, we start %d threads for testing)...\n", thread_num);
     log_handler = flog_create("multip_thread");
-    pthread_t tid1, tid2, tid3;
-    int end = num / 3;
-    pthread_create(&tid1, NULL, write_log, (void*)&end);
-    pthread_create(&tid2, NULL, write_log, (void*)&end);
-    pthread_create(&tid3, NULL, write_log, (void*)&end);
-    pthread_join(tid1, NULL);
-    pthread_join(tid2, NULL);
-    pthread_join(tid3, NULL);
+    pthread_t tid[thread_num];
+    int end = num / thread_num;
+
+    for ( int i = 0; i < thread_num; i++ ) {
+        pthread_create(&tid[i], NULL, write_log, (void*)&end);
+    }
+
+    for ( int i = 0; i < thread_num; i++ ) {
+        pthread_join(tid[i], NULL);
+    }
 
     sleep(2);
 }
