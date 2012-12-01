4b108d4e3d887ebbb069c1e97d063a94a66d7c67
huyuzhang <hyzwowtools@gmail.com>
2012-12-01 19:19:54 +0800

    adjust config

--- httpd_mock/start_httpd_mock.sh
+++ httpd_mock/start_httpd_mock.sh
@@ -3,7 +3,8 @@
 ulimit -c unlimited;
 ulimit -n 65535;
 ulimit -a;
-sysctl -w net.ipv4.ip_local_port_range="18000 61000";
+sysctl -w net.ipv4.ip_local_port_range="15000 61000";
 sysctl -w net.ipv4.tcp_tw_reuse=1;
 
 ./httpd_mock -c default_httpd_mock.cfg
+# valgrind --tool=memcheck --leak-check=full ./httpd_mock -c default_httpd_mock.cfg
